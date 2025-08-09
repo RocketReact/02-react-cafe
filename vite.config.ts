90f707c8a38bf6256248edaa7109f6f805fc4eae
Oleg Shapoval <olegkazhan@gmail.com>
2025-08-09 16:55:05 +0300

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    [
+      react(),
+      svgr({
+        include: "**/*.svg",
+      }),
+    ],
+  ],
   base:
     process.env.NODE_ENV === "production"
       ? process.env.VERCEL
