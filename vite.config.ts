e99b9e9176e40272e28fbf7950c3a6fed548b5a2
Oleg Shapoval <olegkazhan@gmail.com>
2025-08-09 16:36:39 +0300

    init configured Vite+TS+Versel Deploy

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  base:
+    process.env.NODE_ENV === "production"
+      ? process.env.VERCEL
+        ? "/"
+        : "/my-react-practice/"
+      : "./",
+  build: {
+    sourcemap: true,
+  },
+});
